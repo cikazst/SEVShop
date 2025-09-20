a5404822b025171873615ec544bba7326e43ca1e
Cika <cikazhrasy@gmail.com>
2025-09-20 20:05:38 +0700

    bismillah

---
+++ Page/shopAll.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php require_once '../Component/header.php'; ?>
+    <body>
+        <?php require_once '../Component/navbar.php'; ?>
+       <?php require_once '../Component/heroo.php'; ?>
+
+  <div class="konten-produk">
+    <div class="judul-kategori">
+      <h1>Best Seller</h1>
+    </div>
+
+    <div id="merch" class="tab-isi">
+      <div class="grid-produk">
+
+        <div class="tampilan-produk-kartu kartu-spesial">
+          <div class="gambar-produk"><img src="/produk cover/10cm doll.png" alt=""></div>
+          <div class="nama-produk">Teenieping 10cm Doll</div>
+          <div class="jenis-harga">IDR</div>
+          <div class="harga-produk-best">Rp. 260.000,00</div>
+        </div>
+
+        <div class="tampilan-produk-kartu kartu-spesial">
+          <div class="gambar-produk"><img src="/produk cover/cahol set.png" alt=""></div>
+          <div class="nama-produk">Photo Card Holder Set</div>
+          <div class="jenis-harga">IDR</div>
+          <div class="harga-produk-best">Rp. 220.000,00</div>
+        </div>
+
+       <div class="tampilan-produk-kartu kartu-spesial">
+          <div class="gambar-produk"><img src="/produk cover/cahol set.png" alt=""></div>
+          <div class="nama-produk">Photo Card Holder Set</div>
+          <div class="jenis-harga">IDR</div>
+          <div class="harga-produk-best">Rp. 220.000,00</div>
+        </div>
+
+        <div class="tampilan-produk-kartu kartu-spesial">
+          <div class="gambar-produk"><img src="/produk cover/cahol set.png" alt=""></div>
+          <div class="nama-produk">Photo Card Holder Set</div>
+          <div class="jenis-harga">IDR</div>
+          <div class="harga-produk-best">Rp. 220.000,00</div>
+        </div>
+
+        <div class="tampilan-produk-kartu kartu-spesial">
+          <div class="gambar-produk"><img src="/produk cover/cahol set.png" alt=""></div>
+          <div class="nama-produk">Photo Card Holder Set</div>
+          <div class="jenis-harga">IDR</div>
+          <div class="harga-produk-best">Rp. 220.000,00</div>
+        </div>
+
+         <div class="tampilan-produk-kartu kartu-spesial">
+          <div class="gambar-produk"><img src="/produk cover/10cm doll.png" alt=""></div>
+          <div class="nama-produk">Teenieping 10cm Doll</div>
+          <div class="jenis-harga">IDR</div>
+          <div class="harga-produk-best">Rp. 260.000,00</div>
+
+        </div> 
+        <div class="tampilan-produk-kartu kartu-spesial">
+          <div class="gambar-produk"><img src="/produk cover/10cm doll.png" alt=""></div>
+          <div class="nama-produk">Teenieping 10cm Doll</div>
+          <div class="jenis-harga">IDR</div>
+          <div class="harga-produk-best">Rp. 260.000,00</div>
+        </div>
+           <div class="tampilan-produk-kartu kartu-spesial">
+          <div class="gambar-produk"><img src="/produk cover/10cm doll.png" alt=""></div>
+          <div class="nama-produk">Teenieping 10cm Doll</div>
+          <div class="jenis-harga">IDR</div>
+          <div class="harga-produk-best">Rp. 260.000,00</div>
+        
+        </div>
+      </div>
+    </div>
+  </div>
+
+        <?php require_once '../Component/footer.php'; ?>
+    </body>
+</html>
